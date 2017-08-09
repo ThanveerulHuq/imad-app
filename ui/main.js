5ed37c7e046cac91d8c1f04ca3dfa4372668f3ce
Thanveer <thanveersiddiq@gmail.com>
2017-08-09 13:14:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+var button=document.getElemenById('counter');
+button.onclick= function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readuState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter =request.responseText;
+                var span= document.getElemenById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://thanveersiddiq.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
