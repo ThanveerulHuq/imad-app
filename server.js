7d29b7aaeb54a7cf2c27410adbdbad878cc519cd
Thanveer <thanveersiddiq@gmail.com>
2017-08-09 12:59:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,9 +54,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
 app.get('/article-one', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
+app.get('/counter', function (req, res) {
   res.send(CreateTemplate(articleOne));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
