30fcf8b726b87f83261a601867add135d29d05fe
Thanveer <thanveersiddiq@gmail.com>
2017-08-22 15:03:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,16 +55,22 @@ function CreateTemplate(data){
 }
 
 var pool=new Pool(config);
-app.get('/test-db', function (req, res) {
-    pool.query('SELECT * FROM user',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send(JSON.stringify(result));
-        }
-        
-    });
-  
+
+app.get('/test-db',function(req,res){
+   
+   pool.query('SELECT * from User',function(err,result)
+   {
+       if(err)
+       {
+           res.status(500).send(err.toString());
+           
+       }
+       else
+       {
+           res.send(JSON.stringify(result));
+       }
+   });
+   
 });
 
 
