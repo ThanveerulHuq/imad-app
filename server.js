4d7de226478d20521b2be627fa7bd4c33d47fdcc
Thanveer <thanveersiddiq@gmail.com>
2017-08-28 11:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'thanveersiddiq',
     database:'thanveersiddiq',
@@ -72,12 +73,19 @@ app.get('/test-db',function(req,res){
    });
    
 });
+function hash(input,salt){
+    var hashed= crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    return hashed.toString('hex');
+}
 
+app.get('/hash/:input', function (req, res) {
+    var hashed=hash(req.params.input,'this-is-a-random-string');
+  res.send(hashed);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 var counter=0;
 app.get('/article-one', function (req, res) {
   res.send(CreateTemplate(articleOne));
