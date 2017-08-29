ccdaf822140f22212a00580991281b99559104b5
Thanveer <thanveersiddiq@gmail.com>
2017-08-29 13:15:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/login', function (req, res) {
                var salt=dbString.split('$')[2];
                var hashedPassword=hash(password,salt);
                if(hashedPassword === dbString){
-                   req.session.auth={userId:resul.rows[0].id};
+                   req.session.auth={userId:result.rows[0].id};
                    res.send("credentials correct");
                }else{
                    res.send(403).send('username/password is invalid');
