4364aba88e858315040114dbcf11a31707046374
Thanveer <thanveersiddiq@gmail.com>
2017-08-29 15:07:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,12 @@ app.get('/check-login', function (req, res) {
         res.send('You are not logged in');
     }
 });
+app.get('/logout', function (req, res) {
+  delete req.session.auth;
+  res.send('Logged out successfully');
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
